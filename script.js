6435bf4be0a80db1158c81881ac78f02a5b76cf8
Akashkr1090 <akashkumar120032011090@gmail.com>
2022-09-29 22:11:26 +0530

    Sticky header

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {myFunction()};
+
+var navlist = document.getElementById("navlist");
+var sticky = navlist.offsetTop;
+
+/* Function to stick the nav bar */
+function myFunction() 
+{
+	if (window.pageYOffset >= sticky) {
+		navlist.classList.add("sticky")
+	}
+	else 
+        {
+		    navlist.classList.remove("sticky");
+		}
+}
